ad4c89e2e08480c3c7030bedb666feaec60ea290
Seth <sethmiller@student.olympic.edu>
2025-04-09 17:49:26 -0700

    Create MyFavoriteThings.py
    Decribes my favorite things

---
+++ MyFavoriteThings.py
@@ -0,0 +1,13 @@
+def main ():
+    MyFavoriteThings = [
+            "Working on cars",
+            "Building Motors",
+            "Hanging out with family",
+            "Working"
+    ]
+  print("Here are a few of my favorite things:")
+    for thing in favorite_things:
+        print(f"- {thing}")
+
+if __name__ == "__main__":
+    main()
