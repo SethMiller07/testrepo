79961e8984a7d478d04a9ed8cbe074fd9db7a0a1
Seth <sethmiller@student.olympic.edu>
2025-04-20 19:34:42 -0700

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,6 @@
+import math
+first_number = input("what is the first number you would like to add?")
+print("okay,",first_number)
+second_number = input("what would you like to have for your second number?")
+print("okay,", second_number)
+print(first_number+second_number)
