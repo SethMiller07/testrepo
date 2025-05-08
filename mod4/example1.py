24f6cb0e6985cb18774e5f77c0790f9b64e3f9e2
Seth <sethmiller@student.olympic.edu>
2025-05-07 22:29:55 -0700

    Create example1.py

---
+++ mod4/example1.py
@@ -0,0 +1,6 @@
+# 1. Truth table for (A AND B) OR (NOT B)
+# A      B      (A AND B)    (NOT B)    (A AND B) OR (NOT B)
+# True   True   True         False      True
+# True   False  False        True       True
+# False  True   False        False      False
+# False  False  False        True       True
