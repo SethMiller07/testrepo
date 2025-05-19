476c94e55c050b0f93e57e8e1e131b32d573dec3
Seth <sethmiller@student.olympic.edu>
2025-05-18 21:39:48 -0700

    Create problem2.py

---
+++ mod6/problem2.py
@@ -0,0 +1,6 @@
+words = ["Olympic", "Games", "Olympic", "Torch", "Winter", "Olympic", "Sports"]
+count = 0
+for word in words:
+    if word == "Olympic":
+        count += 1
+print("The word 'Olympic' appears", count, "times.")
