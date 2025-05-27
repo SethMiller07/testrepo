0ccb9106ed34561d4753049941052e13cb63ca1c
Seth <sethmiller@student.olympic.edu>
2025-05-26 19:29:56 -0700

    Create number1.py

---
+++ mod7/number1.py
@@ -0,0 +1,10 @@
+def count_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in input_string:
+        if char in vowels:
+            count += 1
+    return count
+
+text = ("hello my name is seth")
+print(count_vowels(text))
